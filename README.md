7b3d8483a9c2aacac171081730239fc95b41c96e
Som's Laptop <somaiahuthappa@gmail.com>
2022-01-26 16:15:47 -0500

    added-code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Password-Generator
+
+## Password generator that creates a password based on the following parameters:
+- Number of characters
+- Uppercase included
+- Lowercase included
+- Numbers included
+- Special characters included
+
+## Features
+- Allows you to cancel function at the start
+- Alerts that guide the user if they have missed a step or not followed instructions
+
+## Links
+- https://github.com/somaiahuthappa/Password-Generator
+- https://somaiahuthappa.github.io/Password-Generator/
+
+
+## Screenshots
+- ![This is a screenshot](/assets/images/screenshot1.PNG)
+- ![This is a screenshot](/assets/images/screenshot2.PNG)
--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,5 +1,63 @@
 // Assignment code here
 
+// generate password function
+function generatePassword() {
+  var charCount = characterCount();
+  var choiceChars = "";
+  var password = "";
+
+  // function will run if characterCount() is not null.
+  if (charCount != null) {
+
+    // select uppercase
+    var selectUpper = confirm("Do you want UPPERCASE LETTER in your password?");
+
+    if (selectUpper) {
+      choiceChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    // select lowercase 
+    var selectLower = confirm("Do you want LOWERCASE LETTERS in your password?");
+    if (selectLower) {
+      choiceChars += "abcdefghijklmnopqrstuvwxyz";
+    }
+    // select numbers
+    var selectNumbers = confirm("Do you want NUMBERS in your password?");
+    if (selectNumbers) {
+      choiceChars += "1234567890";
+    }
+    // select special characters
+    var selectSpecial = confirm("Do you want SPECIAL CHARACTERS in your password?");
+    if (selectSpecial) {
+      choiceChars += "!@#$%^&~()_+}{:/<.,>=-";
+    }
+    // alert if none of the above parameters have been selected
+    if (choiceChars.length == 0) {
+      alert("Please choose at least one.");
+      return generatePassword();
+    };
+    // generate random combination
+    for (var i = 0; i < charCount; i++) {
+      password += choiceChars.charAt(Math.floor(Math.random() * choiceChars.length));
+    }
+  }
+  return password;
+}
+
+// function to select password length
+function characterCount() {
+  var count = 0;
+
+  while (count < 8 || count > 125) {
+    count = prompt("Please enter a number between 8 and 125");
+    // if statement to prevent a letter selected
+    if (isNaN(count)) { count = 0; }
+    // if statement to break the loop
+    if (count === null) { break; }
+  }
+
+  return count;
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +73,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
